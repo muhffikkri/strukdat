6b8ab062edcc2a41e1df7252359a11ca88ca35f9
Nexvaulus <moehammad.fikr@gmail.com>
2025-09-08 15:54:08 +0700

    menambahkan materi tabel

---
+++ 24060124130069_SD02/P02_Tabel/mtabel.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "tabel.h"
+/ *Deskripsi : driver ADT tabel integer * /
+	/* NIM/Nama : */
+	/* Tanggal : */
+
+	int main()
+{
+	/*kamus*/
+
+	/*algoritma*/
+	// createTable(&T1);
+	// addXTable(&T1,'A');
+	// printTable(T1);
+	// addXTable(&T1,'B');
+	// delXTable(&T1,'A');
+	// printTable(T1);
+	// dst ...
+	return 0;
+}
---
+++ 24060124130069_SD02/P02_Tabel/tabel.c
@@ -0,0 +1,8 @@
+#ifndef tabel_c
+#define tabel_c
+#include "tabel.h"
+/* Deskripsi : */
+/* NIM/Nama : */
+/* Tanggal : */
+
+#endif
