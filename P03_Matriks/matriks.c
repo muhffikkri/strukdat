96f1239a3124f9bd969c88b8b19f18915a95fe29
Nexvaulus <moehammad.fikr@gmail.com>
2025-09-17 02:37:20 +0700

    Add Tabel and Matriks modules with basic operations
    - Implemented Tabel module with functions for creating, manipulating, and querying a character table.
    - Added Matriks module with functions for matrix initialization, manipulation, and arithmetic operations.
    - Created header files for both modules to define their interfaces.
    - Developed driver files for testing the functionalities of the Tabel and Matriks modules.
    - Included documentation for each function and procedure in the respective header files.

---
+++ P03_Matriks/matriks.c
@@ -0,0 +1,8 @@
+/* Program   : matriks.c */
+/* Deskripsi : file BODY modul matriks integer */
+/* NIM/Nama  : */
+/* Tanggal   : */
+/***********************************/
+
+#include <stdio.h>
+/* include matriks.h & boolean.h */
---
+++ P03_Matriks/mmatriks.c
@@ -0,0 +1,15 @@
+/* Program   : mmatriks.c */
+/* Deskripsi : driver ADT matriks integer */
+/* NIM/Nama  : */
+/* Tanggal   : */
+/***********************************/
+
+#include <stdio.h>
+
+int main() {
+	/*kamus*/
+	
+	/*algoritma*/
+	
+	return 0;
+}
