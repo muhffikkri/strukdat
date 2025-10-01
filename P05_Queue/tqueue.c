469d50dbe8488f85f127039c6f04fd511b3bd471
Nexvaulus <moehammad.fikr@gmail.com>
2025-10-02 00:52:37 +0700

    Add tqueue and tqueue2 implementations for contiguous queue ADTs

---
+++ P05_Queue/tqueue.c
@@ -0,0 +1,205 @@
+#include <stdio.h>
+#include "tqueue.h"
+#include "boolean.h"
+
+/* Program   : tqueue.h */
+/* Deskripsi : ADT Queue representasi kontigu dengan array,
+               model I: head selalu di posisi 0 atau 1 */
+/* NIM/Nama  : */
+/* Tanggal   : */
+/***********************************/
+
+/*procedure createQueue ( output Q:tQueue)
+{I.S.: -}
+{F.S.: Q terdefinisi, kosong}
+{Proses: mengisi elemen dengan huruf '-', head=tail=0 }*/
+void createQueue(tqueue *Q)
+{
+    int i = 1;
+
+    for (i; i <= 5; i++)
+    {
+        (*Q).wadah[i] = '-';
+    }
+
+    (*Q).head = 0;
+    (*Q).tail = 0;
+}
+
+/*function Head(Q:tQueue)-> integer
+{mengembalikan elemen terdepan antrian Q} */
+// int Head(tqueue Q);
+#define head(Q) (Q).head // implementasi fisik macro
+
+/*function Tail(Q:tQueue)-> integer
+{mengembalikan elemen terakhir antrian Q} */
+// int Tail(tqueue Q);
+#define tail(Q) (Q).tail // implementasi fisik macro
+
+/*function infoHead(Q:tQueue)-> character
+{mengembalikan nilai elemen terdepan antrian Q} */
+/*pikirkan bila antrian kosong*/
+int infoHead(tqueue Q)
+{
+    return Q.wadah[head(Q)];
+}
+
+/*function infoTail(Q:tQueue)-> character
+{mengembalikan nilai elemen terakhir antrian Q} */
+/*pikirkan bila antrian kosong*/
+int infoTail(tqueue Q)
+{
+    return Q.wadah[tail(Q)];
+}
+
+/*function sizeQueue(Q:tQueue)-> integer
+{mengembalikan panjang antrian Q} */
+int sizeQueue(tqueue Q)
+{
+    return tail(Q);
+}
+
+/*procedure printQueue(input Q:tQueue)
+{I.S.: Q terdefinisi}
+{F.S.: -}
+{proses: mencetak semua elemen wadah ke layar}*/
+void printQueue(tqueue Q)
+{
+    int i = 1;
+
+    for (i; i <= 5; i++)
+    {
+        printf("%c", Q.wadah[i]);
+    }
+}
+
+/*procedure viewQueue(input Q:tQueue)
+{I.S.: Q terdefinisi}
+{F.S.: -}
+{proses: mencetak elemen tak kosong ke layar}*/
+void viewQueue(tqueue Q)
+{
+    int i = 1;
+
+    if (!isEmptyQueue(Q))
+    {
+        for (i; i <= tail(Q); i++)
+        {
+            printf("%c", Q.wadah[i]);
+        }
+    }
+}
+
+/*function isEmptyQueue(Q:tQueue) -> boolean
+{mengembalikan true jika Q kosong}*/
+boolean isEmptyQueue(tqueue Q)
+{
+    return head(Q) == 0 && tail(Q) == 0;
+}
+
+/*function isFullQueue(Q:tQueue) -> boolean
+{mengembalikan true jika Q penuh}*/
+boolean isFullQueue(tqueue Q)
+{
+    return head(Q) == 1 && tail(Q) == 5;
+}
+
+/*function isOneElement(Q:tQueue) -> boolean
+{mengembalikan true jika hanya ada 1 elemen }*/
+boolean isOneElement(tqueue Q)
+{
+    return head(Q) == 1 && tail(Q) == 1;
+}
+
+/*procedure enqueue( input/output Q:tQueue, input e: character )
+{I.S.: Q dan e terdefinisi}
+{F.S.: elemen wadah Q bertambah 1, bila belum penuh}
+{proses: menambah elemen wadah Q } */
+void enqueue(tqueue *Q, char e)
+{
+    if (!isFullQueue(*Q))
+    {
+        (*Q).wadah[tail(*Q) + 1] = e;
+
+        if (isEmptyQueue(*Q))
+        {
+            (*Q).head = 1;
+        }
+        (*Q).tail = tail(*Q) + 1;
+    }
+}
+
+/*procedure deQueue( input/output Q:tQueue, output e: character )
+{I.S.: }
+{F.S.: e=infohead(Q) atau e='-' bila Q kosong, elemen wadah Q berkurang 1 }
+{proses: mengurangi elemen wadah Q, semua elemen di belakang head digeser maju }
+{bila awalnya 1 elemen, maka Head dan Tail menjadi 0 } */
+void dequeue(tqueue *Q, char *e)
+{
+    int i = 1;
+
+    if (!isEmptyQueue(*Q))
+    {
+        (*e) = infoHead(*Q);
+
+        for (i; i <= tail(*Q) - 1; i++)
+        {
+            (*Q).wadah[i] = (*Q).wadah[i + 1];
+        }
+        (*Q).wadah[tail(*Q)] = '-';
+        (*Q).tail = tail(*Q) - 1;
+
+        if (tail(*Q) == 0)
+        {
+            (*Q).head = 0;
+        }
+    }
+}
+
+/*tambahan, dikerjakan bila cukup waktu */
+
+/*procedure enqueue2( input/output Q1:tQueue, input/output Q2:tQueue, input e: character )
+{I.S.: e terdefinisi}
+{F.S.: elemen wadah Q1 atau Q2 bertambah 1, bila belum penuh}
+{proses: menambah elemen wadah pada antrian terpendek dari Q1 atau Q2} */
+void enqueue2(tqueue *Q1, tqueue *Q2, char e)
+{
+    if ((sizeQueue(*Q1) < sizeQueue(*Q2)) && !isFullQueue(*Q1))
+    {
+        enqueue(&(*Q1), e);
+    }
+    else if ((sizeQueue(*Q1) > sizeQueue(*Q2)) && !isFullQueue(*Q2))
+    {
+        enqueue(&(*Q2), e);
+    }
+    else
+    {
+        if (!isFullQueue(*Q1) && !isFullQueue(*Q2))
+        {
+            enqueue(&(*Q1), e);
+            enqueue(&(*Q2), e);
+        }
+    }
+}
+
+/*procedure dequeue2( input/output Q1:tQueue, input/output Q2:tQueue, output e: character )
+{I.S.: }
+{F.S.: e=infohead Q1 atau Q2 atau e='-' bila Q1 dan Q2 kosong, elemen wadah Q1 atau Q2 berkurang 1 }
+{proses: mengurangi elemen wadah antrian terpanjang Q1 atau Q2, semua elemen di belakang head digeser maju }
+{bila awalnya 1 elemen, maka Head dan Tail antrian menjadi 0 } */
+void dequeue2(tqueue *Q1, tqueue *Q2, char *e)
+{
+    if ((sizeQueue(*Q1) < sizeQueue(*Q2)) && !isEmptyQueue(*Q2))
+    {
+        dequeue(&(*Q2), &(*e));
+    }
+    else if ((sizeQueue(*Q1) > sizeQueue(*Q2)) && !isEmptyQueue(*Q1))
+    {
+        dequeue(&(*Q1), &(*e));
+    }
+    else
+    {
+        dequeue(&(*Q1), &(*e));
+        dequeue(&(*Q2), &(*e));
+    }
+}
--- P05_Queue/tqueue2.c
+++ P05_Queue/tqueue2.c
@@ -1,103 +1,179 @@
-#ifndef tqueue2_H
-#define tqueue2_H
-#include "boolean.h" //ambil dari praktikum sebelumnya
+#include <stdio.h>
+#include "tqueue.h"
+#include "boolean.h"
 
 /* Program   : tqueue2.h */
-/* Deskripsi : ADT Queue representasi kontigu dengan array, 
+/* Deskripsi : ADT Queue representasi kontigu dengan array,
                model II: head bergeser/jelajah */
 /* NIM/Nama  : */
 /* Tanggal   : */
 /***********************************/
 
 /* type tQueue2 = < wadah: array [1..5] of character ,
-				head: integer ,
-				tail: integer >
+                head: integer ,
+                tail: integer >
 {cara akses: Q:tQueue2, Q.head = head(Q) ...} */
-typedef struct { char wadah[6];  //kapasitas 5 elemen, indeks 0 tidak dipakai 
-                  int head; 
-                  int tail; 
-				} tqueue2;
+typedef struct
+{
+    char wadah[6]; // kapasitas 5 elemen, indeks 0 tidak dipakai
+    int head;
+    int tail;
+} tqueue2;
 
 /*function isEmptyQueue2(Q:tQueue2) -> boolean
 {mengembalikan true jika Q kosong}*/
-boolean isEmptyQueue2(tqueue2 Q);
- 
+boolean isEmptyQueue2(tqueue2 Q)
+{
+    return head2(Q) == 0 && tail2(Q) == 0;
+}
+
 /*function isFullQueue2(Q:tQueue2) -> boolean
 {mengembalikan true jika Q penuh}*/
-boolean isFullQueue2(tqueue2 Q);
+boolean isFullQueue2(tqueue2 Q)
+{
+    return head2(Q) == 1 && tail2(Q) == 5;
+}
 
 /*function isOneElement2(Q:tQueue2) -> boolean
 {mengembalikan true jika Q berisi 1 elemen}*/
-boolean isOneElement2(tqueue2 Q);
+boolean isOneElement2(tqueue2 Q)
+{
+    return head2(Q) == tail2(Q) && head2(Q) != 0 && tail2(Q) != 0;
+}
 
 /*procedure createQueue2 ( output Q:tQueue2 )
 {I.S.: -}
 {F.S.: Q terdefinisi, kosong}
-{Proses: mengisi head dan tail dengan 0, elemen kosong='#'}*/ 
-void createQueue2(tqueue2 *Q);
+{Proses: mengisi head dan tail dengan 0, elemen kosong='#'}*/
+void createQueue2(tqueue2 *Q)
+{
+    int i = 1;
+
+    for (i; i <= 5; i++)
+    {
+        (*Q).wadah[i] = '#';
+    }
+
+    (*Q).head = 0;
+    (*Q).tail = 0;
+}
 
 /*Function Head2(Q:Tqueue2) -> integer
 {mengembalikan posisi elemen terdepan} */
-int head2(tqueue2 Q);
+int head2(tqueue2 Q)
+{
+    return Q.head;
+}
 
 /*Function Tail2(Q:Tqueue2) -> integer
 {mengembalikan posisi elemen terakhir} */
-int tail2(tqueue2 Q);
+int tail2(tqueue2 Q)
+{
+    return Q.tail;
+}
 
 /*Function InfoHead2(Q:Tqueue2) -> character
 {mengembalikan nilai elemen terdepan} */
-char infoHead2(tqueue2 Q);
+char infoHead2(tqueue2 Q)
+{
+    return Q.wadah[head2(Q)];
+}
 
 /*Function InfoTail2(Q:Tqueue2) -> character
 {mengembalikan nilai elemen terakhir} */
-char infoTail2(tqueue2 Q);
+char infoTail2(tqueue2 Q)
+{
+    return Q.wadah[tail2(Q)];
+}
 
-/*function sizeQueue2(Q:tQueue2)-> integer 
+/*function sizeQueue2(Q:tQueue2)-> integer
 {mengembalikan panjang antrian Q} */
-int sizeQueue2(tqueue2 Q);
+int sizeQueue2(tqueue2 Q)
+{
+    return tail2(Q) - head2(Q) + 1;
+}
 
 /*procedure printQueue2(input Q:tQueue2)
 {I.S.: Q terdefinisi}
 {F.S.: -}
 {proses: mencetak semua isi wadah ke layar}*/
-void printQueue2(tqueue2 Q);
+void printQueue2(tqueue2 Q)
+{
+    int i = 1;
+
+    for (i; i <= 5; i++)
+    {
+        printf("%c", Q.wadah[i]);
+    }
+}
 
 /*procedure viewQueue2(input Q:tQueue2)
 {I.S.: Q terdefinisi}
 {F.S.: -}
 {proses: mencetak elemen yang tidak kosong ke layar}*/
-void viewQueue2(tqueue2 Q);
+void viewQueue2(tqueue2 Q)
+{
+    int i = head2(Q);
+
+    if (!isEmptyQueue2(Q))
+    {
+        for (i; i <= tail(Q); i++)
+        {
+            printf("%c", Q.wadah[i]);
+        }
+    }
+}
 
 /*Function IsTailStop(Q:TQueue2) -> boolean
 {mengembalikan true jika Tail tidak dapat lagi geser}
 {karena sudah di posisi kapasitas} */
-boolean isTailStop(tqueue2 Q);
+boolean isTailStop(tqueue2 Q)
+{
+    tail2(Q) == 5;
+}
 
 /*Procedure ResetHead(input/output Q:TQueue2)
 {I.S:Tail=kapasitas, head>1; F.S:head=1 }
 {Proses: mengembalikan Head ke indeks 1 }
 {Elemen selain head ikut bergeser menyesuaikan} */
 /*CATATAN: di praktikum ini, resetHead dilakukan ketika Enqueue */
-void resetHead(tqueue2 *Q);
+void resetHead(tqueue2 *Q)
+{
+
+    int i = 1, j = head2(*Q);
+
+    if (tail2(*Q) == 5)
+    {
+        for (i; i <= sizeQueue2(*Q); i++)
+        {
+            (*Q).wadah[i] = (*Q).wadah[j];
+            (*Q).wadah[j] = '#';
+        }
+    }
+    (*Q).head = 1;
+    (*Q).tail = sizeQueue2(*Q);
+}
 
 /*procedure enQueue2( input/output Q:tQueue2, input E: character )
 {I.S.: E terdefinisi}
 {F.S.: elemen wadah Q bertambah 1 bila belum penuh}
-{proses: menambah elemen wadah Q, jika tail(Q)=kapasitas, 
+{proses: menambah elemen wadah Q, jika tail(Q)=kapasitas,
 maka semua elemen digeser lebih dulu sehingga head(Q)=1 } */
-void enqueue2(tqueue2 *Q, char E);
-  
+void enqueue2(tqueue2 *Q, char E)
+{
+}
+
 /*procedure deQueue2( input/output Q:tQueue2, output E: character )
 {I.S.: }
 {F.S.: elemen wadah Q berkurang 1 (Head), E=infohead(Q) lama, bila kosong, E='@'}
-{proses: mengurangi elemen wadah Q, bila 1 elemen, 
+{proses: mengurangi elemen wadah Q, bila 1 elemen,
 maka Head dan Tail mengacu ke 0 } */
 void dequeue2(tqueue2 *Q, char *E);
 
 /*procedure enQueue2N( input/output Q:tQueue2, input N:integer )
 {I.S.: Q terdefinisi, mungkin kosong, N <= kapasitas - panjang antrean}
 {F.S.: elemen wadah Q bertambah <= N elemen bila belum penuh}
-{proses: mengisi elemen dari keyboard, jika tail(Q) mencapai kapasitas, 
+{proses: mengisi elemen dari keyboard, jika tail(Q) mencapai kapasitas,
 maka semua elemen digeser lebih dulu sehingga head(Q)=1 } */
 void enqueue2N(tqueue2 *Q, int N);
 
@@ -106,5 +182,3 @@ void enqueue2N(tqueue2 *Q, int N);
 {mengembalikan true jika Q1 dan Q2 berisi elemen yang sama}
 {ingat, kondisi head Q1 dan Q2 mungkin tidak sama} */
 boolean isEqualQueue2(tqueue2 Q1, tqueue2 Q2);
-
-#endif
